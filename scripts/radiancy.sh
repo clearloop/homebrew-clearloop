4cb0270af2987147c84102270c7b890ebe419afd
mercury <udtrokia@gmail.com>
2018-06-24 01:15:23 +0800

    lightly

--- scripts/radiancy.sh
+++ scripts/radiancy.sh
@@ -1,15 +1,5 @@
 # build.sh
 
-# clone radiancy
-git clone https://github.com/udtrokia/Radiancy.git --depth=1
+(cargo) &> /dev/null || (curl https://sh.rustup.rs -sSf | sh)
 
-# cd path
-cd radiancy
-
-cargo &> /dev/null || (curl https://sh.rustup.rs -sSf | sh)
-
-cargo build --release
-
-# path
-path=`pwd`
-echo "PATH=\$PATH:$path/radiancy/target/release" >> ~/.bashrc
+cargo install radiancy
