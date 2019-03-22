73426064c5be1e87fc030b53fc32b4ee0d4dc148
clearloop <udtrokia@gmail.com>
2019-03-22 14:04:41 +0800

    [Update]: Update cita scripts

--- cita.rb
+++ cita.rb
@@ -1,14 +1,18 @@
-# github: https://github.com/cryptape/cita
+# CITA Formula
+# Offical Repository: https://github.com/cryptape/cita
+# # #
 class Cita < Formula
   desc "A high performance blockchain for enterprise users."
   homepage "https://docs.citahub.com/en-US/welcome"  
-  url "https://github.com/clearloop/cita/releases/download/formula%400.21/cita_0.21_ed25519.zip"
-  sha256 "b108126375ed67af24b5387adcdf127280ef06d420e3b07d5646fcb68fa1a4d7"
+  url "https://github.com/clearloop/cita/releases/download/cita%400.21/cita_0.21_ed25519.zip"
+  sha256 "d43b328130bfe0917e3c5b566246e324d1d4071ca1568f1a223a14c131c09684"
   version "0.21"
   
   def install
     libexec.install Dir["*"]
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink Dir["#{libexec}/bin/cita"]
+    bin.install_symlink Dir["#{libexec}/bin/cita-env"]
+    bin.install_symlink Dir["#{libexec}/bin/cita-config"]
   end
 
   def caveats; <<~EOS
